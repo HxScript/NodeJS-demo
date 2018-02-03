27e3769ad508662dacc3dfb90e47d1619acedacc
Long <hxscript@163.com>
2018-02-03 16:47:51 +0800

    add

---
+++ DAO/app.js
@@ -0,0 +1,54 @@
+// Copyright (c) 2018 by HxScript. All Rights Reserved.
+
+var express = require("express")
+var app = express()
+var db = require("./db.js")
+
+
+app.get("/", function(req,res) {
+db.insertOne("student", {"name":"baba"},function(err,result) {
+  if(err) {
+    console.log("插入失败")
+    return
+  }
+  res.send("成功啦！")
+})
+
+})
+
+app.get("/fenye1", function(req,res) {
+  var page = parseInt(req.query.page);
+  var message = []
+  db.findAll("student", {}, function(err, result) {
+
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    for(var i = page * 5; i < (page + 1) * 5; i++) {
+      message.push(result[i])
+    }
+    res.send(message)
+  })
+})
+
+
+
+
+
+app.get("/fenye2", function(req,res) {
+  var page = parseInt(req.query.page)
+  var step = 5
+  db.findPage("student", {},[(page - 1) * step, step], function(err,result) {
+    if(err) {
+      console.log("分页查找失败")
+      return
+    }
+    res.send(result)
+  })
+})
+
+
+
+app.listen(3000)
--- DAO/db.js
+++ DAO/db.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2018 by HxScript. All Rights Reserved.
 var MongoClient = require("mongodb").MongoClient
 
-function __connectDB(/*mdata*/) {
-  var url = "mongodb://localhost:27017/" //+ mdata
+function __connectDB(/*mdata,*/ callback) {
+  var url = "mongodb://localhost:27017/cug" //+ mdata
   MongoClient.connect(url, function(err,db) {
     //回调函数表示连接数据库成功之后要做的事情，db即连接上的数据库实体
     if(err) {
@@ -11,7 +11,78 @@ function __connectDB(/*mdata*/) {
     }
     console.log("connected correctly to server.")
     // assert.equal(null, err)
+    callback(err, db)
   })
 }
 
-__connectDB()
+exports.insertOne = function(collectionName, json, callback) {
+  if(arguments.length != 3) {
+    callback("insertOne函数要有3个参数", null)
+    return
+  }
+  console.log("db成功")
+  __connectDB(function(err,db) {
+    db.collection(collectionName).insertOne(json, function(err,result) {
+      callback(err,result)
+      db.close()
+    })
+    //连接成功之后做的事情
+  })
+}
+
+exports.findAll = function(collectionName, json, callback) {
+  var result = []   //结果数组
+  if(arguments.length != 3) {
+    callback("find函数要有3个参数", null)
+    return
+  }
+
+  __connectDB(function(err,db) {
+    var cursor = db.collection(collectionName).find(json)
+    cursor.each(function(err,doc) {
+      if(err) {
+        callback(err,null)
+        return
+      }
+      if (doc != null) {
+        result.push(doc)
+      } else {
+        callback(null, result)
+      }
+    })
+  })
+}
+
+// skip 是一个数组，接受两个参数。一个是起始页，一个是步进数。
+exports.findPage = function(collectionName, json, skip, callback) {
+  // 结果
+  var result = []
+  // 给回掉函数返回参数条件错误时的错误
+  if(arguments.length != 4) {
+    callback("find函数要有4个参数", null)
+    return
+  }
+  // 给回调函数返回skip参数错误时的错误
+  if (skip.length != 2) {
+    callback("步进输入错误", null)
+  }
+  var start = skip[0]
+  var step = skip[1]
+
+  __connectDB(function(err,db) {
+    // skip（2）:从数据集的第二条开始查询
+    // limit(2) : 依次查出2条数据。
+    var cursor = db.collection(collectionName).find(json).limit(step).skip(start)
+    cursor.each(function(err,doc) {
+      if(err) {
+        callback(err,null)
+        return
+      }
+      if (doc != null) {
+        result.push(doc)
+      } else {
+        callback(null, result)
+      }
+    })
+  })
+}
