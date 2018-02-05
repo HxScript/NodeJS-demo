eec054d0ae5e008c33e52e17d9d9240421061c4d
Long <hxscript@163.com>
2018-02-05 21:39:57 +0800

    add

--- tree-hole/app.js
+++ tree-hole/app.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var app = express()
 var db = require("./model/db.js")
+var formidable = require("formidable")
 
 app.set("view engine", "ejs")
 
@@ -8,5 +9,25 @@ app.get("/", function(req,res,next) {
   res.render("index")
 
 })
+
+app.post("/tijiao", function(req,res,next) {
+    var form = new formidable.IncomingForm()
+    form.parse(req, function(err, fields) {
+        db.insertOne("treeHole", {
+            "name" : fields.name,
+            "liuyan" : fields.liuyan
+        }, function (err, result) {
+            if(err) {
+                //-1是给ajax看的  失败时-1
+                res.json({"result":"-1"})
+                return
+            }
+            res.json({"result":"1"})
+            console.log(result)
+        })
+        // console.log("recive:" + fields.name)
+    })
+})
+
 
 app.listen(3000,"localhost")
